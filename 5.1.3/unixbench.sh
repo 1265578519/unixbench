6482dca45b2fdab2f96c9406288a8c3c53f4520e
小樱 <1265578519@qq.com>
2015-07-29 05:18:47 +0800

    转入GitHub
    转入GitHub

---
+++ 5.1.3/unixbench.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "========================================================================="
+echo "Thanks for using UnixBench 5.1.3 for CentOS Auto-Install Script"
+echo "========================================================================="
+yum -y install wget make gcc gcc-c automake autoconf libtool gcc-c++ time perl-Time-HiRes SDL-devel mesa-libGL-devel
+wget https://raw.githubusercontent.com/1265578519/unixbench/master/5.1.3/UnixBench5.1.3.tgz
+tar xzf UnixBench5.1.3.tgz
+cd UnixBench
+make
+./Run
+echo "========================================================================="
+echo "test end"
+echo "========================================================================="
+echo ""
+echo "http://bbs.itzmx.com"
+echo ""
+echo "Thank you!"
+echo "========================================================================="
