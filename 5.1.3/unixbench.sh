f20fa54077f80de4c706f39c71fcd716d9b07410
小樱 <1265578519@qq.com>
2015-12-23 17:36:24 +0800

    修改xshell脚本地址为开源中国

--- 5.1.3/unixbench.sh
+++ 5.1.3/unixbench.sh
@@ -3,7 +3,7 @@ echo "========================================================================="
 echo "Thanks for using UnixBench 5.1.3 for CentOS Auto-Install Script"
 echo "========================================================================="
 yum -y install wget make gcc gcc-c automake autoconf libtool gcc-c++ time perl-Time-HiRes SDL-devel mesa-libGL-devel
-wget https://raw.githubusercontent.com/1265578519/unixbench/master/5.1.3/UnixBench5.1.3.tgz
+wget http://git.oschina.net/cc12655/unixbench/raw/master/5.1.3/UnixBench5.1.3.tgz -O UnixBench5.1.3.tgz
 tar xzf UnixBench5.1.3.tgz
 cd UnixBench
 make
